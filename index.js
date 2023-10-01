8f67789c99e8b8ad992f47a8996d88ffc645b074
Mizz <123146044+MrMizzles@users.noreply.github.com>
2023-10-01 15:31:09 -0400

    updated CORS configuration
    added code to reflect recommended best practice using CORS.

--- index.js
+++ index.js
@@ -31,7 +31,28 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+  "https://80sflixs.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // Authentification & Login Endpoint
 let auth = require("./auth")(app); // Login HTML Authentification
